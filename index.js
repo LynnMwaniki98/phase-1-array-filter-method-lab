c32c2a7b6ef1ddda907f2d79f0629049b70d7dfa
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-20 19:12:21 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+let findMatching = (array, name) => {
+    let text = array.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return text !== undefined ? text : undefined;
+}
+
+const fuzzyMatch = (array, letters) => {
+    let text = array.filter(driver => driver.startsWith(letters))
+    return text !== undefined ? text : undefined;
+}
+
+const matchName = (array, name) => {
+    let text = array.filter(driver => driver.name === name);
+    return text !== undefined ? text : undefined;
+}
